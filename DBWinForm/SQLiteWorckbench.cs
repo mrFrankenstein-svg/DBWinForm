7c01b2ca82bac1cfeb5d9009fd7b0810f9577266
mrFrankenstein-svg <vlad.1994@list.ru>
2022-09-18 02:47:03 +0300

    Немного поковырял функции отправки ответа на запрос

--- DBWinForm/SQLiteWorckbench.cs
+++ DBWinForm/SQLiteWorckbench.cs
@@ -30,7 +30,8 @@ namespace DBWinForm
             ConnectToDB();
 
             SQLiteCommand CMD = DB.CreateCommand();
-            CMD.CommandText = "select * from Content where Title like '%" + (string)searcResult.Text + "%' or Content like '%" + (string)searcResult.Text + "%'";
+            //CMD.CommandText = "select * from Content where Title like '%" + (string)searcResult.Text + "%' or Content like '%" + (string)searcResult.Text + "%'";
+            CMD.CommandText = "select *,substr(Content,1,20) from Content where Title like '%" + (string)searcResult.Text + "%' or Content like '%" + (string)searcResult.Text + "%'";
             SQLiteDataReader dbReturnetData = CMD.ExecuteReader();
             if (dbReturnetData.HasRows)
             {
@@ -42,7 +43,7 @@ namespace DBWinForm
                     rowCounterForSearcResult++;
                     searcResult.Items.Insert(rowCounterForSearcResult, (string)dbReturnetData["Title"]);
                     rowCounterForSearcResult++;
-                    searcResult.Items.Insert(rowCounterForSearcResult, (string)dbReturnetData["Content"]);
+                    searcResult.Items.Insert(rowCounterForSearcResult, (string)dbReturnetData["substr(Content,1,20)"] + "...");
                     rowCounterForSearcResult++;
                 }
             }
@@ -86,5 +87,30 @@ namespace DBWinForm
 
             BreakTheConnectionToDB();
         }
+        
+        public List<string> SearchInDBOnServer(string searchQuery)
+        {
+            List<string> searchResult = new List<string>();
+            ConnectToDB();
+            SQLiteCommand CMD = DB.CreateCommand();
+            CMD.CommandText = "select * substr(Content,1,25) from Content where Title like '%" + searchQuery + "%' or Content like '%" + searchQuery + "%'";
+            SQLiteDataReader dbReturnetData = CMD.ExecuteReader();
+            if (dbReturnetData.HasRows)
+            {
+                while (dbReturnetData.Read())
+                {
+                    searchResult.Add(dbReturnetData["ID"].ToString() + " id");
+                    searchResult.Add((string)dbReturnetData["Title"]);
+                    searchResult.Add((string)dbReturnetData["Content"]);
+
+                }
+            }
+            else
+            {
+                searchResult.Add("Нет такго");
+            }
+            BreakTheConnectionToDB();
+            return searchResult;
+        } 
     }
 }
